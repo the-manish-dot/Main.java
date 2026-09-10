9d1dd98b196160d97cc173735bacccc405c83358
the-manish-dot <zwzsan60gmail.com>
2026-09-10 02:38:11 -0700

    push

---
+++ questions/Example.java
@@ -0,0 +1,42 @@
+class Example1{
+    int id;
+    string name;
+    public Example1(int id,string name){
+    this.id=id;
+    this.string=name
+    }
+
+
+
+    //void initize(){
+    //    id=1;
+    //    name="newton"
+    //}
+
+}
+public class example{
+        public static void main(String[] args) {
+            Example1 a =new Example1();
+            a.initize();
+            System.out.println("the name"+a.name);
+            System.out.println("id "+a.id);
+
+}
+}
+//create a book class with the following attribute:
+//bookid
+//auother
+ 
+ //create a parameterized constructor to initialize these valuuse/
+ //craete a method  displayinmforming ()to dispaly the book the detials.
+  //in  main().crate two boook objets using the construtor dispaly theeir
+  //information
+
+
+  //exersion 2
+  //create an empoyeecalss with:
+  //.id
+  //namw
+  //salarly
+  //carete 
+
---
+++ questions/Exersion1.java
@@ -0,0 +1,24 @@
+class Exersion {
+    int bookid ;
+    String title;
+    String auothr name;
+
+    Constructorexersion1(int book id, string title, sitring author name0){
+    this.bookid =bookid;
+    this.title = title;
+    this.author name =author name;
+}
+void dispalyimp(){
+    Systrm.out.println("bookid"+bookid);
+    System.out.println("author"+author);
+    System.out.println("authot name"+author name);
+}
+}
+ public Class ExersionA{
+    public static void main(string[]args){
+        ConstructorExersion1 obj = new exersion1(book id:1, title: your name,author name:Manish );
+        obj.dispalyimp();
+
+    }
+
+ }
