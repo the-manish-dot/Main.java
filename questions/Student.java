675437785299c0629ef00dff857c349503867b19
the-manish-dot <zwzsan60gmail.com>
2026-09-23 02:23:27 -0700

    Your commit message

---
+++ questions/Student.java
@@ -0,0 +1,23 @@
+class Student{
+    int id;
+    int age;
+    String name;
+    Student(int n,int a,String n){
+        id=n;
+        age=a;
+        name=n;
+    }
+      Student(int n,int a,String n){
+    id = i;    
+    name = n;    
+    age=a;    
+    }  
+      Student(int n,int a,String n){
+    id = i;    
+    name = n;    
+    age=a; 
+    void display(){System.out.println(id+" "+name+" "+age)
+    }
+
+}
+}
